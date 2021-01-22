6cb42edadf315b6d74417f855c44d5f22cef58ca
brad-waterhouse <bradley.waterhouse@bunches.co.uk>
2021-01-22 14:00:18 +0000

    adds basic header image to homepage

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,32 +3,48 @@ import { ReactElement } from "react";
 import { Helmet } from "react-helmet";
 
 import "./../assets/scss/App.scss";
+import "./../assets/scss/home.scss";
 
 const Home = (): ReactElement => {
   return (
     <>
       <Helmet>
-        <title>TITLE</title>
+        <title>Ruben & Me | Home</title>
         <meta name="description" content="" />
       </Helmet>
+
+      <header className="masthead">
+        <div className="top-padding">
+          <div className="columns is-multiline has-text-centered">
+            <div className="column is-12">
+              <h2 className="title">Ruben & Me.</h2>
+              <h3 className="subtitle">personalisation done personally.</h3>
+            </div>
+          </div>
+        </div>
+      </header>
 
       <main className="main-content">
         <div className="container">
           <div className="columns is-multiline has-text-centered">
             <div className="column is-12">
-              <h2 className="title">TITLE</h2>
-              <h3 className="subtitle">SUBTITLE</h3>
-            </div>
-          </div>
-          <div className="columns">
-            <div className="column is-4 is-offset-4">
-              <figure className="image">
-                <img
-                  className="is-rounded"
-                  src="https://via.placeholder.com/640"
-                  alt=""
-                />
-              </figure>
+              <div className="columns is-multiline is-mobile">
+                <div className="column is-one-fifth-desktop is-12-mobile">
+                  <code>is-one-quarter</code>
+                </div>
+                <div className="column is-one-fifth-desktop is-12-mobile">
+                  <code>is-one-quarter</code>
+                </div>
+                <div className="column is-one-fifth-desktop is-12-mobile">
+                  <code>is-one-quarter</code>
+                </div>
+                <div className="column is-one-fifth-desktop is-12-mobile">
+                  <code>is-one-quarter</code>
+                </div>
+                <div className="column is-one-fifth-desktop is-12-mobile">
+                  <code>is-one-quarter</code>
+                </div>
+              </div>
             </div>
           </div>
         </div>
