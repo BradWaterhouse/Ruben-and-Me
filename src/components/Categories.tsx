908366eeae47cbeb505143a9a2865b0cf64696ab
brad-waterhouse <bradley.waterhouse@bunches.co.uk>
2021-02-05 14:53:20 +0000

    adds over to category buttons

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -1,9 +1,5 @@
 import React, { ReactElement } from "react";
 
-interface Props {}
-
-interface State {}
-
 const Categories = (): ReactElement => {
   return (
     <>
@@ -14,112 +10,136 @@ const Categories = (): ReactElement => {
           <div className="columns is-multiline is-mobile">
             <div className="column is-one-fifth-desktop is-12-mobile is-4-tablet">
               <div className="card" style={{ paddingBottom: 8 }}>
-                <img
-                  src="https://cdn.my1styears.com/media/wysiwyg/home/grid/ENewIn.jpg"
-                  alt="Alps"
-                />
-                <div className="w3-container w3-center">
-                  <h3>Some Product Titles</h3>
-                </div>
+                <a href="/" className="category-button">
+                  <img
+                    src="https://cdn.my1styears.com/media/wysiwyg/home/grid/ENewIn.jpg"
+                    alt="Alps"
+                  />
+                  <div className="w3-container w3-center">
+                    <h3>Some Product Titles</h3>
+                  </div>
+                </a>
               </div>
             </div>
             <div className="column is-one-fifth-desktop is-12-mobile is-4-tablet">
               <div className="card" style={{ paddingBottom: 8 }}>
-                <img
-                  src="https://cdn.my1styears.com/media/wysiwyg/home/grid/ESoftToys.jpg"
-                  alt="Alps"
-                />
-                <div className="w3-container w3-center">
-                  <h3>Some Product Titles</h3>
-                </div>
+                <a href="/" className="category-button">
+                  <img
+                    src="https://cdn.my1styears.com/media/wysiwyg/home/grid/ESoftToys.jpg"
+                    alt="Alps"
+                  />
+                  <div className="w3-container w3-center">
+                    <h3>Some Product Titles</h3>
+                  </div>
+                </a>
+              </div>
+            </div>
+
+            <div className="column is-one-fifth-desktop is-12-mobile is-4-tablet">
+              <div className="card" style={{ paddingBottom: 8 }}>
+                <a href="/" className="category-button">
+                  <a href="/" className="category-button">
+                    <img
+                      src="https://cdn.my1styears.com/media/wysiwyg/home/grid/20_01_21_Homepage_Tile_Category_Block_Robes.jpg"
+                      alt="Alps"
+                    />
+                    <div className="w3-container w3-center">
+                      <h3>Some Product Titles</h3>
+                    </div>
+                  </a>
+                </a>
+              </div>
+            </div>
+
+            <div className="column is-one-fifth-desktop is-12-mobile is-4-tablet">
+              <div className="card" style={{ paddingBottom: 8 }}>
+                <a href="/" className="category-button">
+                  <img
+                    src="https://cdn.my1styears.com/media/wysiwyg/home/grid/20_01_21_Homepage_Tile_Category_Block_GiftSets.jpg"
+                    alt="Alps"
+                  />
+                  <div className="w3-container w3-center">
+                    <h3>Some Product Titles</h3>
+                  </div>
+                </a>
               </div>
             </div>
             <div className="column is-one-fifth-desktop is-12-mobile is-4-tablet">
               <div className="card" style={{ paddingBottom: 8 }}>
-                <img
-                  src="https://cdn.my1styears.com/media/wysiwyg/home/grid/20_01_21_Homepage_Tile_Category_Block_Robes.jpg"
-                  alt="Alps"
-                />
-                <div className="w3-container w3-center">
-                  <h3>Some Product Titles</h3>
-                </div>
+                <a href="/" className="category-button">
+                  <img
+                    src="https://cdn.my1styears.com/media/wysiwyg/home/grid/EBackpacks.jpg"
+                    alt="Alps"
+                  />
+                  <div className="w3-container w3-center">
+                    <h3>Some Product Titles</h3>
+                  </div>
+                </a>
               </div>
             </div>
             <div className="column is-one-fifth-desktop is-12-mobile is-4-tablet">
               <div className="card" style={{ paddingBottom: 8 }}>
-                <img
-                  src="https://cdn.my1styears.com/media/wysiwyg/home/grid/20_01_21_Homepage_Tile_Category_Block_GiftSets.jpg"
-                  alt="Alps"
-                />
-                <div className="w3-container w3-center">
-                  <h3>Some Product Titles</h3>
-                </div>
+                <a href="/" className="category-button">
+                  <img
+                    src="https://cdn.my1styears.com/media/wysiwyg/home/grid/EBackpacks.jpg"
+                    alt="Alps"
+                  />
+                  <div className="w3-container w3-center">
+                    <h3>Some Product Titles</h3>
+                  </div>
+                </a>
               </div>
             </div>
             <div className="column is-one-fifth-desktop is-12-mobile is-4-tablet">
               <div className="card" style={{ paddingBottom: 8 }}>
-                <img
-                  src="https://cdn.my1styears.com/media/wysiwyg/home/grid/EBackpacks.jpg"
-                  alt="Alps"
-                />
-                <div className="w3-container w3-center">
-                  <h3>Some Product Titles</h3>
-                </div>
+                <a href="/" className="category-button">
+                  <img
+                    src="https://cdn.my1styears.com/media/wysiwyg/home/grid/20_01_21_Homepage_Tile_Category_Block_Robes.jpg"
+                    alt="Alps"
+                  />
+                  <div className="w3-container w3-center">
+                    <h3>Some Product Titles</h3>
+                  </div>
+                </a>
               </div>
             </div>
             <div className="column is-one-fifth-desktop is-12-mobile is-4-tablet">
               <div className="card" style={{ paddingBottom: 8 }}>
-                <img
-                  src="https://cdn.my1styears.com/media/wysiwyg/home/grid/EBackpacks.jpg"
-                  alt="Alps"
-                />
-                <div className="w3-container w3-center">
-                  <h3>Some Product Titles</h3>
-                </div>
+                <a href="/" className="category-button">
+                  <img
+                    src="https://cdn.my1styears.com/media/wysiwyg/home/grid/EWoodenToys.jpg"
+                    alt="Alps"
+                  />
+                  <div className="w3-container w3-center">
+                    <h3>Some Product Titles</h3>
+                  </div>
+                </a>
               </div>
             </div>
             <div className="column is-one-fifth-desktop is-12-mobile is-4-tablet">
               <div className="card" style={{ paddingBottom: 8 }}>
-                <img
-                  src="https://cdn.my1styears.com/media/wysiwyg/home/grid/20_01_21_Homepage_Tile_Category_Block_Robes.jpg"
-                  alt="Alps"
-                />
-                <div className="w3-container w3-center">
-                  <h3>Some Product Titles</h3>
-                </div>
+                <a href="/" className="category-button">
+                  <img
+                    src="https://cdn.my1styears.com/media/wysiwyg/home/grid/EStorage.jpg"
+                    alt="Alps"
+                  />
+                  <div className="w3-container w3-center">
+                    <h3>Some Product Titles</h3>
+                  </div>
+                </a>
               </div>
             </div>
             <div className="column is-one-fifth-desktop is-12-mobile is-4-tablet">
               <div className="card" style={{ paddingBottom: 8 }}>
-                <img
-                  src="https://cdn.my1styears.com/media/wysiwyg/home/grid/EWoodenToys.jpg"
-                  alt="Alps"
-                />
-                <div className="w3-container w3-center">
-                  <h3>Some Product Titles</h3>
-                </div>
-              </div>
-            </div>
-            <div className="column is-one-fifth-desktop is-12-mobile is-4-tablet">
-              <div className="card" style={{ paddingBottom: 8 }}>
-                <img
-                  src="https://cdn.my1styears.com/media/wysiwyg/home/grid/EStorage.jpg"
-                  alt="Alps"
-                />
-                <div className="w3-container w3-center">
-                  <h3>Some Product Titles</h3>
-                </div>
-              </div>
-            </div>
-            <div className="column is-one-fifth-desktop is-12-mobile is-4-tablet">
-              <div className="card" style={{ paddingBottom: 8 }}>
-                <img
-                  src="https://cdn.my1styears.com/media/wysiwyg/home/grid/ENewIn.jpg"
-                  alt="Alps"
-                />
-                <div className="w3-container w3-center">
-                  <h3>Some Product Titles</h3>
-                </div>
+                <a href="/" className="category-button">
+                  <img
+                    src="https://cdn.my1styears.com/media/wysiwyg/home/grid/ENewIn.jpg"
+                    alt="Alps"
+                  />
+                  <div className="w3-container w3-center">
+                    <h3>Some Product Titles</h3>
+                  </div>
+                </a>
               </div>
             </div>
           </div>
